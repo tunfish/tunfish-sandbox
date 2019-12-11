383cd2797d25c10b0a1c0fe8edb57c98c10e147e
Andreas Motl <andreas@tunfish.org>
2019-12-11 23:58:40 +0100

    Add setup.py, use script entrypoints

---
+++ setup.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+requires = [
+    'autobahn',
+    'crossbar',
+    'sqlalchemy',
+    'sqlalchemy-utils',
+    #'psycopg2',
+    'pyroute2',
+    'msgpack',
+    'pysodium',
+    'python-iptables',
+]
+
+extras = {
+}
+
+setup(name='tunfish',
+      version='0.1.0',
+      description='Convenient VPN infrastructure on top of secure WireGuard tunnels',
+      long_description=README,
+      license="AGPL 3, EUPL 1.2",
+      classifiers=[
+        "Programming Language :: Python 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "Topic :: Communications",
+        "Topic :: Database",
+        "Topic :: Internet",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: Software Development :: Embedded Systems",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Object Brokering",
+        "Topic :: System :: Networking",
+        "Topic :: Utilities",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS"
+        ],
+      author='The Tunfish Developers',
+      author_email='hello@tunfish.org',
+      url='https://github.com/tunfish/tunfish-system',
+      keywords='',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={
+      },
+      zip_safe=False,
+      test_suite='tunfish.test',
+      install_requires=requires,
+      extras_require=extras,
+      dependency_links=[
+      ],
+      entry_points={
+          'console_scripts': [
+              'tf-client           = tunfish.main:start_client',
+              'tf-gateway          = tunfish.main:start_gateway',
+              'tf-server           = tunfish.main:start_server',
+          ],
+      },
+)
--- src/tunfish/main.py
+++ src/tunfish/main.py
@@ -3,15 +3,6 @@ import sys
 VERSION = '0.1.0'
 
 
-def show_options():
-    print(f"--help      show help")
-    print(f"--version   show version")
-    print(f" ")
-    print(f"--server    start as server")
-    print(f"--gateway   start as gateway")
-    print(f"--client    start as client")
-
-
 def show_version():
     print(f"Version: {VERSION}")
 
@@ -24,33 +15,15 @@ def start_server():
     server.start()
 
 
-def start_client(name):
+def start_client():
+    name = sys.argv[1]
     from tunfish.client import TunfishClient
     client = TunfishClient()
     client.start(name)
 
 
-def start_gateway(name):
+def start_gateway():
+    name = sys.argv[1]
     from tunfish.gateway import TunfishGateway
     gateway = TunfishGateway()
     gateway.start(name)
-
-
-def main():
-    for arg in sys.argv[1:]:
-        if arg == '--server':
-            start_server()
-        elif arg == '--client':
-            start_client(sys.argv[2])
-        elif arg == '--gateway':
-            start_gateway(sys.argv[2])
-        elif arg == '--help':
-            show_options()
-        elif arg == '--version':
-            show_version()
-        else:
-            show_options()
-
-
-if __name__ == '__main__':
-    main()
